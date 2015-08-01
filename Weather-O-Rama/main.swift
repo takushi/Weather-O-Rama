65ffbf1dc4d5a3ac5199fa0b05f3a4bd099858fd
takushi <takushi@mfmf.me>
2015-08-01 16:20:46 +0900

    Initial Commit

---
+++ Weather-O-Rama/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Weather-O-Rama
+//
+//  Created by Homma Takushi on 2015/08/01.
+//  Copyright (c) 2015年 mfmf.me. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
